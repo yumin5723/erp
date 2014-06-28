3dfec083b51a05ec43be0c904a18d3bbba27bd9d
liuwanglei <liuwanglei2001@163.com>
2014-06-28 10:46:58 +0800

    update nev prod

---
+++ invertory/environments/dev/backend/config/main-local.php
@@ -0,0 +1,12 @@
+<?php
+return [
+	'components'=>[
+		'db' => [
+			'class' => 'yii\db\Connection',
+			'dsn' => 'mysql:host=127.0.0.1;dbname=yltd',
+			'username' => 'root',
+			'password' => '',
+			'charset' => 'utf8',
+		],
+	],
+];
---
+++ invertory/environments/dev/common/config/params-local.php
@@ -0,0 +1,6 @@
+<?php
+return [
+	'attachDir'=>'/data0/static_files',
+	'targetDomain'=>'http://static.goumin.com',
+	'resource_folder'=>"/data0/goumindata/static_files/",
+];
