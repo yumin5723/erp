4580f73ba8289b5d45e54a809fda5df23418d960
liuwanglei <liuwanglei@goumin.com>
2014-12-16 14:31:39 +0800

    update user login

--- invertory/customer/controllers/SiteController.php
+++ invertory/customer/controllers/SiteController.php
@@ -53,11 +53,10 @@ class SiteController extends Controller
 		$model = new LoginForm();
 		if ($model->load($_POST) && $model->login()) {
 			return $this->goBack();
-		} else {
-			return $this->render('login', [
-				'model' => $model,
-			]);
 		}
+		return $this->render('login', [
+			'model' => $model,
+		]);
 	}
 
 	public function actionLogout()
